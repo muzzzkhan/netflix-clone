fe8a6deff962fbb0bb098cf8621d301c6168f9e4
Muzammil Khan <97302150+muzzzkhan@users.noreply.github.com>
2022-08-08 22:31:08 +0500

    commit

---
+++ src/request.js
@@ -0,0 +1,12 @@
+const key = '4f535ec765399ed725fac4a7f6a2734e'
+
+{/*Add back ticks ` to use key variable in requests. Other wise manually type the whole key in place of ${key}*/}
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+}
+
+export default requests
